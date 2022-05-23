464a02fe7e0fef2094be38281d919cf1021d7726
KreskoLab <redeezol@gmail.com>
2022-05-23 16:48:40 +0300

    chore: connect google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Overpass:wght@400;500&display=swap',
+        },
+      ],
+    },
+  },
 })
